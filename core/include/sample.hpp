1b140e79f65aa768006da0ca260d3d5a5b89ead7
Madhav Menon <madhavanandmenon@gmail.com>
2026-08-11 14:21:15 +0530

    Fix doxygen comments and header guards for Sample

--- core/include/sample.hpp
+++ core/include/sample.hpp
@@ -1,12 +1,13 @@
-#ifndef TREE_INFER_SAMPLE_H
-#define TREE_INFER_SAMPLE_H
+#ifndef TREEINFER_SAMPLE_H
+#define TREEINFER_SAMPLE_H
 #include <vector>
 
 namespace TreeInfer {
     /**
-     * @brief A vector of feature values for the sample to score
+     * @brief A vector of feature values for the sample to score. Every index i holds the value for whatever feature feature_index == i refers to in every Node across the model
      */
     struct Sample {
+        /// @brief Vector that holds the feature values for the sample to score
         std::vector<float> values;
     };
 }
