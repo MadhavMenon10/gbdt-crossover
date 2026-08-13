0c75e6bd764b62aaf9a2841f86d1b4bd6789b5f6
Madhav Menon <madhavanandmenon@gmail.com>
2026-08-13 16:18:18 +0530

    Fix initialisation for a class

--- core/src/dense_model.cpp
+++ core/src/dense_model.cpp
@@ -1,7 +1,7 @@
 #include "dense_model.hpp"
 #include "dense_builder.hpp"
 
-TreeInfer::DenseModel::DenseModel(const Model& model, std::uint16_t max_depth): max_depth_(max_depth), num_classes_(num_classes), base_scores_(model.base_scores()) {
+TreeInfer::DenseModel::DenseModel(const Model& model, std::uint16_t max_depth): max_depth_(max_depth), num_classes_(model.num_classes()), base_scores_(model.base_scores()) {
     for (const auto& tree : model.trees()) {
         dense_trees_.push_back(generate_dense_tree(tree, max_depth_));
     }
