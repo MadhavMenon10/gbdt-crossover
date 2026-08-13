85e7dd96adbf24100123bc6f9c6da368e7b9afa4
Madhav Menon <madhavanandmenon@gmail.com>
2026-08-13 16:14:36 +0530

    Add missing namespace qualifier

--- cuda/src/launch_tree_traversal_device.cu
+++ cuda/src/launch_tree_traversal_device.cu
@@ -1,4 +1,5 @@
 #include "launch_tree_traversal_device.cuh"
+#include "tree_traversal_device.cuh"
 #include "cuda_utils.cuh"
 
 std::vector<float> TreeInfer::launch_dense_tree_traversal_kernel(const DenseModel& dense_model, const std::vector<TreeInfer::Sample>& samples) {
@@ -26,7 +27,7 @@ std::vector<float> TreeInfer::launch_dense_tree_traversal_kernel(const DenseMode
     size_t num_output_elements {num_samples * num_trees};
     CUDAUtils::check_cuda_error(cudaMalloc(reinterpret_cast<void**>(&output_array_d), sizeof(float) * num_output_elements), "Allocate memory for output_array_d");
     size_t block_size {256};
-    traverse_dense_tree_device<<<(num_output_elements + block_size - 1) / block_size, block_size>>>(dense_nodes_d, feature_values_d, nodes_per_tree, num_samples, num_trees, features_per_sample, output_array_d);
+    TreeInfer::traverse_dense_tree_device<<<(num_output_elements + block_size - 1) / block_size, block_size>>>(dense_nodes_d, feature_values_d, nodes_per_tree, num_samples, num_trees, features_per_sample, output_array_d);
     std::vector<float> output_vector(num_output_elements);
     CUDAUtils::check_cuda_error(cudaMemcpy(output_vector.data(), output_array_d, sizeof(float) * num_output_elements, cudaMemcpyDeviceToHost), "Move output elements into the output vector");
     CUDAUtils::check_cuda_error(cudaFree(dense_nodes_d), "Free dense_nodes_d");
